ab969b6f61d08d710483059dbcd3cbaa80b78ee3
Abhijith P <blackdevilhacker4@gmail.com>
2024-05-23 18:15:22 +0530

    blog app

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
-      <p>Powered by{" @"}<a href="/BlOG-PAGE" target="_blank" style={{textDecoration:"none"}}>Anonymous</a></p>
+      <p>Powered by{" @"}<a href="/" target="_blank" style={{textDecoration:"none"}}>Anonymous</a></p>
     </footer>
   );
 };
